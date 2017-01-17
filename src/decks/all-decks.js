da34166144758ddb79e0e5454a2efc70646c25c0
Nadir Kadem <nadir.kadem@gmail.com>
2017-01-17 22:53:44 +0100

    add cage deck and image handling

---
+++ src/decks/all-decks.js
@@ -0,0 +1,15 @@
+import hiragana from './hiragana.js';
+import katakana from './katakana.js';
+import capitals from './capitals.js';
+import basicFrench from './basic-french.js';
+import facesOfCage from './faces-of-cage.js';
+
+const decks = [
+  facesOfCage,
+  hiragana,
+  katakana,
+  capitals,
+  basicFrench
+];
+
+export default decks;
---
+++ src/decks/faces-of-cage.js
@@ -0,0 +1,23 @@
+const deck = {
+    name: 'Faces of Cage',
+    description: 'Guess the movie Nicolas Cage plays in',
+    fuzzy: true,
+    cards: [
+         {q: '/images/cage/the-rock.jpg', a: 'The Rock'},
+         {q: '/images/cage/raising-arizona.jpg', a: 'Raising Arizona'},
+         {q: '/images/cage/season-of-the-witch.jpg', a: 'Season of the Witch'},
+         {q: '/images/cage/snake-eyes.png', a: 'Snake Eyes'},
+         {q: '/images/cage/benjamin-gates.jpg', a: 'Benjamin Gates'},
+         {q: '/images/cage/birdy.jpg', a: 'Birdy'},
+         {q: '/images/cage/con-air.png', a: 'Con Air'},
+         {q: '/images/cage/face-off.jpg', a: 'Face/Off'},
+         {q: '/images/cage/ghost-rider.jpg', a: 'Ghost Rider'},
+         {q: '/images/cage/lord-of-war.jpg', a: 'Lord of War'},
+         {q: '/images/cage/8mm.jpg', a: '8mm'},
+         {q: '/images/cage/adaptation.jpg', a: 'Adaptation'},
+         {q: '/images/cage/bad-lieutenant.jpg', a: 'Bad Lieutenant'},
+         {q: '/images/cage/bangkok-dangerous.jpg', a: 'Bangkgok Dangerous'},
+    ]
+};
+
+export default deck;
