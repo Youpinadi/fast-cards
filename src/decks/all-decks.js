88d9ab239f1771a8d14e8c97d476d18709bd9ac6
Nadir Kadem <nadir.kadem@gmail.com>
2017-02-06 21:53:49 +0100

    add spanish deck

--- src/decks/all-decks.js
+++ src/decks/all-decks.js
@@ -3,13 +3,15 @@ import katakana from './katakana.js';
 import capitals from './capitals.js';
 import basicFrench from './basic-french.js';
 import facesOfCage from './faces-of-cage.js';
+import spanishTop100 from './spanish-top-100.js';
 
 const decks = [
   facesOfCage,
   hiragana,
   katakana,
   capitals,
-  basicFrench
+  basicFrench,
+  spanishTop100
 ];
 
 export default decks;
